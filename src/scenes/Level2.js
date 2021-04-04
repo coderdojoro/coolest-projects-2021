6e48df4be9f3070aab61f7268c8139ede9788bbb
Mihai Vasilache <mihai.vasilache@edenred.com>
2021-04-04 12:29:07 +0300

    replace id with gid for wolf name

--- src/scenes/Level2.js
+++ src/scenes/Level2.js
@@ -134,13 +134,13 @@ class Level2 extends Phaser.Scene {
       if (object.type == 'wolf') {
         let wolf = new Wolf(this, object.x, object.y);
         this.physics.add.collider(wolf, this.groundLayer, wolf.groundColided, null, wolf);
-        wolf.setName('wolf-' + object.id);
+        wolf.setName('wolf-' + object.gid);
         this.wolfGroup.add(wolf, false);
       }
       if (object.type == 'beholder') {
         let beholder = new Beholder(this, object.x, object.y);
         this.physics.add.collider(beholder, this.groundLayer, beholder.groundColided, null, beholder);
-        beholder.setName('beholder-' + object.id);
+        beholder.setName('beholder-' + object.gid);
         this.beholderGroup.add(beholder, false);
       }
     }
