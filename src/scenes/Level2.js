ab5a19c7ac441ef743fd5f572a75aa5944830710
Mihai Vasilache <mihai.vasilache@edenred.com>
2021-04-25 20:31:10 +0300

    merge

--- src/scenes/Level2.js
+++ src/scenes/Level2.js
@@ -12,23 +12,25 @@ class Level2 extends Phaser.Scene {
   }
 
   preload() {
-    this.load.tilemapTiledJSON('level1-tilemap', 'assets/level2-tilemap.json');
+    this.load.tilemapTiledJSON('level2-tilemap', 'assets/level2-tilemap.json');
 
-    this.load.image('ground-image', 'assets/tiles/level2-tiles.png');
-    this.load.spritesheet('bush-image', 'assets/tiles/level2-bush.png', {
+    this.load.image('lvl2-ground-image', 'assets/tiles/level2-tiles.png');
+    this.load.spritesheet('lvl2-bush-image', 'assets/tiles/level2-bush.png', {
       frameWidth: 32,
       frameHeight: 32,
     });
 
-    this.load.image('trees-image', 'assets/tiles/level2-trees.png');
+    this.load.image('lvl2-trees-image', 'assets/tiles/level2-trees.png');
 
-    this.load.image('background4', 'assets/wallpapers/snowy-forest/background4.png');
-    this.load.image('background3', 'assets/wallpapers/snowy-forest/background3.png');
-    this.load.image('background2', 'assets/wallpapers/snowy-forest/background2.png');
-    this.load.image('background1', 'assets/wallpapers/snowy-forest/background1.png');
+    this.load.image('lvl2-background4', 'assets/wallpapers/snowy-forest/background4.png');
+    this.load.image('lvl2-background3', 'assets/wallpapers/snowy-forest/background3.png');
+    this.load.image('lvl2-background2', 'assets/wallpapers/snowy-forest/background2.png');
+    this.load.image('lvl2-background1', 'assets/wallpapers/snowy-forest/background1.png');
 
     this.load.spritesheet('brazier', 'assets/tiles/brazier.png', { frameWidth: 32, frameHeight: 32 });
     this.load.spritesheet('banner', 'assets/tiles/banner.png', { frameWidth: 32, frameHeight: 64 });
+
+    this.load.audio('music-lvl2', 'assets/music-lvl2.mp3');
   }
 
   create() {
@@ -46,7 +48,7 @@ class Level2 extends Phaser.Scene {
       repeat: -1,
     });
 
-    this.map = this.make.tilemap({ key: 'level1-tilemap' });
+    this.map = this.make.tilemap({ key: 'level2-tilemap' });
 
     let heroX;
     let heroY;
@@ -60,10 +62,10 @@ class Level2 extends Phaser.Scene {
       }
     }
 
-    this.background4 = this.map.addTilesetImage('wallpaper4', 'background4');
-    this.background3 = this.map.addTilesetImage('wallpaper3', 'background3');
-    this.background2 = this.map.addTilesetImage('wallpaper2', 'background2');
-    this.background1 = this.map.addTilesetImage('wallpaper1', 'background1');
+    this.background4 = this.map.addTilesetImage('wallpaper4', 'lvl2-background4');
+    this.background3 = this.map.addTilesetImage('wallpaper3', 'lvl2-background3');
+    this.background2 = this.map.addTilesetImage('wallpaper2', 'lvl2-background2');
+    this.background1 = this.map.addTilesetImage('wallpaper1', 'lvl2-background1');
 
     this.battlegroundLayer1 = this.map.createLayer('wallpaper1' /*layer name from json*/, this.background1);
     this.battlegroundLayer1.setScrollFactor(0.0, 1);
@@ -74,15 +76,17 @@ class Level2 extends Phaser.Scene {
     this.battlegroundLayer4 = this.map.createLayer('wallpaper4' /*layer name from json*/, this.background4);
     this.battlegroundLayer4.setScrollFactor(0.6, 1);
 
-    this.groundTiles = this.map.addTilesetImage('ground', 'ground-image');
-    this.bushTiles = this.map.addTilesetImage('bush', 'bush-image');
-    this.treesTiles = this.map.addTilesetImage('trees', 'trees-image');
+    this.groundTiles = this.map.addTilesetImage('ground', 'lvl2-ground-image');
+    this.bushTiles = this.map.addTilesetImage('bush', 'lvl2-bush-image');
+    this.treesTiles = this.map.addTilesetImage('trees', 'lvl2-trees-image');
 
     let backgroundLayer = this.map.createLayer('background' /*layer name from json*/, [this.groundTiles, this.bushTiles, this.treesTiles]);
     this.groundLayer = this.map.createLayer('ground' /*layer name from json*/, [this.groundTiles, this.bushTiles, this.treesTiles]);
     this.hero = new Knight(this, heroX, heroY);
 
     let spikeGroup = this.physics.add.group({ immovable: true, allowGravity: false });
+    this.wolfGroup = this.physics.add.group({ allowGravity: false });
+    this.beholderGroup = this.physics.add.group({ allowGravity: false });
 
     let offX = 5;
     let offY = 8
@@ -92,7 +96,7 @@ class Level2 extends Phaser.Scene {
     for (let a = 0; a < objects.length; a++) {
       let object = objects[a];
       if (object.type == 'spike') {
-        let spike = spikeGroup.create(object.x, object.y, 'bush-image', object.gid - this.bushTiles.firstgid);
+        let spike = spikeGroup.create(object.x, object.y, 'lvl2-bush-image', object.gid - this.bushTiles.firstgid);
         spike.setOrigin(0, 1);
         spike.setAngle(object.rotation);
 
@@ -109,29 +113,30 @@ class Level2 extends Phaser.Scene {
           spike.body.setSize(height, width);
           spike.body.setOffset(- 32 + offY, 32 - offX - width);
         } else {
-          console.error("spike at incorrect angle: " + object.rotation);
+          console.error('spike at incorrect angle: ' + object.rotation);
         }
       }
       if (object.type == 'brazier') {
-        let brazier = this.physics.add.sprite(object.x, object.y, 'bush-image', object.gid - this.bushTiles.firstgid);
+        let brazier = this.physics.add.sprite(object.x, object.y, 'lvl2-bush-image', object.gid - this.bushTiles.firstgid);
         brazier.setOrigin(0, 1);
-        brazier.anims.play("brazier");
+        brazier.anims.play({ key: 'brazier', startFrame: Phaser.Math.Between(0, 5) });
         brazier.body.immovable = true;
         brazier.body.setAllowGravity(false);
         brazier.setScale(2);
       }
       if (object.type == 'banner') {
-        let banner = this.physics.add.sprite(object.x, object.y, 'bush-image', object.gid - this.bushTiles.firstgid);
+        let banner = this.physics.add.sprite(object.x, object.y, 'lvl2-bush-image', object.gid - this.bushTiles.firstgid);
         banner.setOrigin(0, 1);
-        banner.anims.play("banner");
+        banner.anims.play('banner');
         banner.body.immovable = true;
         banner.body.setAllowGravity(false);
-        banner.setScale(3);
+        banner.setScale(1.5);
       }
       if (object.type == 'wolf') {
         let wolf = new Wolf(this, object.x, object.y);
         this.physics.add.collider(wolf, this.groundLayer, wolf.groundColided, null, wolf);
-        wolf.setName('wolf-' + object.gid);
+        wolf.setName('wolf-' + object.id);
+        this.wolfGroup.add(wolf, false);
       }
     }
 
@@ -151,11 +156,22 @@ class Level2 extends Phaser.Scene {
     //ca să nu dea cu capul de cer
     this.physics.world.setBoundsCollision(true, true, false, true);
 
+    this.music = this.sound.add('music-lvl2', {
+      loop: true,
+      volume: 0.3
+    });
+    this.music.play();
+
     // var debug = this.add.graphics();
     // this.groundLayer.renderDebug(debug, {});
 
   }
 
+  finish() {
+    this.music.stop();
+    this.scene.start('StartScreen');
+    this.scene.remove();
+  }
 
 }
 
